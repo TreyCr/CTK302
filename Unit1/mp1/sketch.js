dd06099d22086c1e2215066757da19eee18898ab
TreyCr <69807876+TreyCr@users.noreply.github.com>
2021-01-26 10:17:20 -0600

    multi

---
+++ Unit1/mp1/sketch.js
@@ -0,0 +1,27 @@
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+  background('black');
+  noStroke();
+
+  fill(0, 0, 100);
+  triangle(18, 18, 18, 360, 130, 360);
+
+  fill('blue');
+  ellipse(256, 100, 200, 100);
+    fill(0, 50, 100);
+  ellipse(300, 130, 200, 100);
+    fill(0, 70, 200);
+  ellipse(353, 49, 100, 50);
+  triangle(178, 199, 168, 186, 140, 231);
+
+  fill('white');
+  text(mouseX + " , " + mouseY, 10, 20);
+
+}
+
+function mouseReleased() {
+  print(mouseX + " , " + mouseY);
+}
