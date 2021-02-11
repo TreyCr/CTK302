c904fa314360fb90ee053650de6aeb393b0212c6
TreyCr <69807876+TreyCr@users.noreply.github.com>
2021-02-11 09:25:40 -0600

    multi

---
+++ Unit2/anim1/sketch.js
@@ -0,0 +1,17 @@
+let x=0;
+
+function setup() {
+  createCanvas(1280, 720);
+}
+
+function draw() {
+  background(0);
+  fill(0, 100, 200);
+  textSize (72);
+  text("Trey", x,  height/2);
+  x=x+5;
+  if (x > width) {
+    x=-100;
+  }
+
+}
