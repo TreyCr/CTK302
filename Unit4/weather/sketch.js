2e2873f0a579fb801deda536af69266374777173
TreyCr <69807876+TreyCr@users.noreply.github.com>
2021-04-03 15:41:23 -0500

    dg

---
+++ Unit4/weather/sketch.js
@@ -0,0 +1,70 @@
+// Note - you must change line 19 to your own APPID to get this to work!
+
+var weather;
+var weatherID = 0; // returned in the JSON weather element
+var myState = 0;
+var x = 0;
+var windspeed = 0 ;
+var temp = 0;
+var cast = 0;
+
+function setup() {
+  createCanvas(400, 400);
+
+  // HERE is the call to get the weather.
+
+  var myCityString = 'https://api.openweathermap.org/data/2.5/weather?q=Sydney,AU,AU&units=imperial&';
+
+  //You can also use "zipcode" - var myJSONString = 'https://api.openweathermap.org/data/2.5/weather?zip=61820,us&units=imperial&';
+
+  var myIDString = 'appid=7986b1fc1f5dc6fe824dddc3b406c533'; // USE YOUR ID HERE, take out the x's!!!
+
+  var myBigString = myCityString + myIDString ;
+
+  loadJSON(myBigString, gotData); // that gotData function happens when JSON comes back.
+
+}
+
+
+function gotData(data) {
+
+  weather = data;
+  console.log(weather); // for debugging purposes, print out the JSON data when we get it.
+  windspeed = weather.wind.speed;
+  temp = weather.main.temp;
+  cast = weather.weather[0].description;
+
+
+}
+
+
+function draw() {
+  switch (myState) {
+    case 0:
+      if (weather) {
+        myState = 1;
+      }
+      break;
+
+    case 1:
+      background(200);
+      fill('black');
+      text("What is the weather in " + weather.name + ", " + weather.sys.country + "?", 20, 20);
+      text("The windspeed is " + windspeed + " mph.", 20, 40);
+      text("The temperature is " + temp + "°F.", 20, 60);
+      text("The cast is " + cast + " right now.", 20, 80);
+
+
+      // cloud
+      fill('white');
+      noStroke();
+      ellipse(x, 300, 200, 100);
+
+      // move the cloud's x position
+      x = x + windspeed;
+      if (x > width) x = 0;
+
+      break;
+
+  }
+}
